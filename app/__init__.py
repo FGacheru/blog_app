b1abe9764a77f1219fc9ea8c8a79583d0cb0daa7
b-ndunda <francisgacheru2001@gmail.com>
2020-12-13 18:09:34 +0300

    initialize flask extensions

--- app/__init__.py
+++ app/__init__.py
@@ -3,9 +3,15 @@ from flask import Flask
 def create_app(config_name):
     app = Flask(__name__)
     
+    #Initializing Flask Extensions
+    bootstrap.init_app(app)
+    db.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
+    simple.init_app(app)
     
-    
-     # Creating the app configurations
+
+    # Creating the app configurations
     app.config.from_object(config_options[config_name])
     
      # Registering the blueprint
@@ -15,3 +21,10 @@ def create_app(config_name):
     
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
+    
+    # configure UploadSet
+    configure_uploads(app,photos)
+    
+
+
+    return app
