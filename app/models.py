5a1cd380f0117905df52024a25e34f8277081db5
b-ndunda <francisgacheru2001@gmail.com>
2020-12-13 18:17:47 +0300

    add users class on the models file

---
+++ app/models.py
@@ -0,0 +1,37 @@
+from . import db
+
+
+
+class User(UserMixin,db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255),index = True)
+    email = db.Column(db.String(255),unique = True,index = True)
+    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    password_secure = db.Column(db.String(255))
+    
+    pitch = db.relationship('Comments', backref='author', lazy='dynamic')
+
+    def __repr__(self):
+        return f'User {self.username}'
+    
+    pass_secure  = db.Column(db.String(255))
+
+    @property
+    def password(self):
+            raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+            self.pass_secure = generate_password_hash(password)
+
+
+    def verify_password(self,password):
+            return check_password_hash(self.pass_secure,password)
+
+    def __repr__(self):
+        return f'User {self.username}'
+    
